4b11acdb07d6945278b5393de7acaf767dd8d8bf
jmacias-ctrl <69021407+jmacias-ctrl@users.noreply.github.com>
2023-10-06 11:32:45 -0300

    Add files via upload

--- antcolonyprocess.py
+++ antcolonyprocess.py
@@ -5,16 +5,28 @@ def setSeed(getSeed):
     return True
 
 def calculate_distances(matrix_distance, ant):
-    total_distance = matrix_distance[0][ant[0]]
-    last_city = ant[1]
-    for i in range(2,len(ant)):
+    total_distance = 0
+    last_city = ant[0]
+    for i in range(1,len(ant)):
         total_distance += matrix_distance[last_city][ant[i]]
         last_city = ant[i]
     total_distance += matrix_distance[0][last_city]
     return total_distance
+    
+def calculate_distance_ant_colony(matrix_distance, num_hormigas, ants_colony):
+    ants_distances = np.zeros(num_hormigas, dtype=float)
+    for i in range(len(ants_colony)):
+        ants_distances[i] = calculate_distances(matrix_distance, ants_colony[i])
+    return ants_distances
+        
+def set_ants_colony(tamanio, num_hormigas,matrix_distance):
+    ants_colony = np.zeros((num_hormigas, tamanio), dtype=int)
+    for i in range(len(ants_colony)):
+        ants_colony[i][0] = np.random.randint(1,tamanio-1)
+    return ants_colony, calculate_distance_ant_colony(matrix_distance, num_hormigas, ants_colony)
 
 def initial_ant(tamanio, matrix_distance):
-    set_hormiga = np.arange(1, tamanio)
+    set_hormiga = np.arange(0, tamanio)
     np.random.shuffle(set_hormiga)
-    return set_hormiga, calculate_distances(matrix_distance ,set_hormiga)
+    return set_hormiga, calculate_distances(matrix_distance, set_hormiga)
     
--- main.py
+++ main.py
@@ -11,7 +11,7 @@ if((len(sys.argv)-1)<6):
 #crear la matriz de distancia D, calcular la distancias entre todos los nodos
 seed = int(sys.argv[1])
 num_hormigas = int(sys.argv[2])
-iteraciones = int(sys.argv[3])
+set_iteraciones = int(sys.argv[3])
 fe_feromona = float(sys.argv[4])
 p_valor_heuristica = float(sys.argv[5])
 prob_limite = float(sys.argv[6])
@@ -30,8 +30,6 @@ for i in range(tamanio):
         coordinates_1 = values[i]
         coordinates_2 = values[j]
         radicante = ((coordinates_2[0]-coordinates_1[0])*(coordinates_2[0]-coordinates_1[0]))+((coordinates_2[1]-coordinates_1[1])*(coordinates_2[1]-coordinates_1[1]))
-        if(radicante==0):
-            radicante = 1
         matrix_distance[i][j] = radicante
 matrix_distance = np.sqrt(matrix_distance)
 
@@ -39,6 +37,20 @@ matrix_distance = np.sqrt(matrix_distance)
 matrix_heuristic = 1/matrix_distance
 
 #generar la matriz de feromona de tamaño de la matriz D, se debe dar un valor inicial Tij0=1/(n°variable/costo(primera solucion)) y el resto 0
-hormiga_inicial, costo_hormiga_inicial = initial_ant(tamanio, matrix_distance)
-matrix_feromona_inicial = np.zeros((tamanio, tamanio), dtype=float)+1/((tamanio+1)*costo_hormiga_inicial)
-print(matrix_feromona_inicial)
+mejor_hormiga, costo_mejor_hormiga = initial_ant(tamanio, matrix_distance)
+
+matrix_feromona_inicial = np.zeros((tamanio, tamanio), dtype=float)+1/((tamanio+1)*costo_mejor_hormiga)
+matrix_feromona = np.copy(matrix_feromona_inicial)
+
+ants_colony, costo_ant_colony = set_ants_colony(tamanio, num_hormigas ,matrix_distance)
+
+for i in range(tamanio):
+    for ant in num_hormigas:
+        prob = np.random.rand()
+        if(prob<prob_limite):
+            get_feromona_city = matrix_feromona[:, ant]
+        else:
+
+#while(iteraciones<=set_iteraciones):
+#    break
+print(ants_colony)
